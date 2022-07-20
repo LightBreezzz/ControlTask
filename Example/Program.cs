b300a0cb602842136bdd27770e754409ebdaca3d
Nikolay <kolya_fedosov1992@example.com>
2022-07-20 14:23:22 +0300

    add Example

--- Example/Program.cs
+++ Example/Program.cs
@@ -8,3 +8,30 @@
 лучше обойтись исключительно массивами.
 */
 
+string[] FirstArray = new string[6] {"-=/", "13", "666", "fg", "e56789-", "dfgdfgdg"};
+string[] SecondArray = new string[FirstArray.Length];
+
+void GetArraysecond(string[] FirstArray, string[] SecondArray)
+{
+    int count = 0;
+    for (int i = 0; i < FirstArray.Length; i++)
+    {
+        if (FirstArray[i].Length <= 3)
+        {
+            SecondArray[count] =  FirstArray[i];
+            count ++;
+        }
+    }
+}
+
+void PrintArray(string[] SecondArray)
+{
+    for (int i = 0; i < SecondArray.Length; i++)
+    {
+        Console.Write($"{SecondArray[i]} ");
+    }
+    Console.WriteLine();
+}
+
+GetArraysecond(FirstArray, SecondArray);
+PrintArray(SecondArray);
